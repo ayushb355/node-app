511886a8b9447a9352ac55eb0439588c1233dfeb
ayush bajoria <ayush.bajoria@ing.nl>
2018-07-16 20:52:04 +0200

    ThirdCommit

--- toDoApp1/routes/index.js
+++ toDoApp1/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs){
